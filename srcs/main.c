679c47bf7af559c4e51bfd00290e73352f40f206
bifrah <benj.ifrah@gmail.com>
2021-11-01 17:26:27 +0100

    save

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 17:34:49 by bifrah            #+#    #+#             */
-/*   Updated: 2021/11/01 16:38:50 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/11/01 17:05:15 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 typedef struct s_env {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win_ptr;
 	void	*img_ptr;
 	void	*img_data;
 	int		bits_per_pixel;
@@ -38,7 +38,9 @@ int	key_hook(int keycode, t_env *env)
 {
 	if (keycode == 97)
 	{
-		my_mlx_pixel_put(env->mlx_win, 5, 5, 0x00FF0000);
+		mlx_destroy_image(env->mlx, env->img_ptr);
+		my_mlx_pixel_put(env->img_data, 5, 5, 0x00FF0000);
+		mlx_put_image_to_window(env->mlx, env->win_ptr, env->img_ptr, 0, 0);
 	}
 	return (0);
 }
@@ -48,11 +50,10 @@ int	main(void)
 	t_env	env;
 
 	env.mlx = mlx_init();
-	env.mlx_win = mlx_new_window(env.mlx, 1920, 1080, "Hello world!");
+	env.win_ptr = mlx_new_window(env.mlx, 1920, 1080, "Hello world !");
 	env.img_ptr = mlx_new_image(env.mlx, 1920, 1080);
 	env.img_data = mlx_get_data_addr(env.img_ptr, &(env.bits_per_pixel),
 			&(env.size_line), &(env.endian));
-	mlx_hook(env.mlx_win, 02, (1L << 0), &key_hook, &env);
-	mlx_put_image_to_window(env.mlx, env.mlx_win, env.img_ptr, 0, 0);
+	mlx_hook(env.win_ptr, 02, (1L << 0), &key_hook, &env);
 	mlx_loop(env.mlx);
 }
