720e6eb766b731d36f14ca0fd61ffba8c13873a9
bifrah <benj.ifrah@gmail.com>
2021-11-03 17:36:15 +0100

    norme

--- srcs/main.c
+++ srcs/main.c
@@ -6,15 +6,14 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 17:34:49 by bifrah            #+#    #+#             */
-/*   Updated: 2021/11/03 16:59:15 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/11/03 17:36:05 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../mlx_linux/mlx.h"
 #include "../mlx_linux/mlx_int.h"
 
-typedef struct	s_env {
-
+typedef struct s_env {
 	void	*mlx;
 	void	*win_ptr;
 	void	*img_data;
@@ -24,22 +23,24 @@ typedef struct	s_env {
 	int		endian;
 }				t_env;
 
-
 void	my_mlx_pixel_put(t_env *img_data, int x, int y, int color)
 {
 	char	*dst;
 
-	dst = img_data->img_ptr + (y * img_data->line_length + x * (img_data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	dst = img_data->img_ptr + (y * img_data->line_length
+			+ x * (img_data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
-// int	key_hook(int keycode, t_env *env)
-// {
-//  	if (keycode == 97)
-//  	{
-//  	}
-//  	return (0);
-// }
+int	key_hook(int keycode, t_env *env)
+{
+	if (keycode == 97)
+	{
+		my_mlx_pixel_put(env, 5, 5, 0x00FF0000);
+		mlx_put_image_to_window(env->mlx, env->win_ptr, env->img_data, 0, 0);
+	}
+	return (0);
+}
 
 // void	img_replace(t_env *mlx, t_env win_ptr, t_env img_ptr, t_env img_data)
 // {
@@ -53,10 +54,8 @@ int	main(void)
 	env.mlx = mlx_init();
 	env.win_ptr = mlx_new_window(env.mlx, 1920, 1080, "Hello world !");
 	env.img_data = mlx_new_image(env.mlx, 1920, 1080);
-	env.img_ptr = mlx_get_data_addr(env.img_data, &env.bits_per_pixel, &env.line_length,
-								&env.endian);
-	my_mlx_pixel_put(&env, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(env.mlx, env.win_ptr, env.img_data, 0, 0);
-	//mlx_hook(env.win_ptr, 02, (1L << 0), &key_hook, &env);
+	env.img_ptr = mlx_get_data_addr(env.img_data, &env.bits_per_pixel,
+			&env.line_length, &env.endian);
+	mlx_hook(env.win_ptr, 2, (1L << 0), &key_hook, &env);
 	mlx_loop(env.mlx);
 }
