c92a9d766c73d882543fe75509dd3159392d2228
bifrah <benj.ifrah@gmail.com>
2021-11-03 20:47:03 +0100

    more define

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,14 +6,14 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 20:27:40 by bifrah            #+#    #+#             */
-/*   Updated: 2021/11/03 20:33:08 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/11/03 20:46:55 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include <unistd.h>
+# include "../libft/libft.h"
 
 typedef struct s_env {
 	void	*mlx;
@@ -32,10 +32,20 @@ typedef struct s_env {
 # define BLUE		0x000000FF
 # define MAGENTA	0x00FF00FF
 
+# define LEFT	65361
+# define UP		65362
+# define RIGHT	65363
+# define DOWN	65364
+
+# define ZOOM_IN	65451
+# define ZOOM_OUT	65453
+
 # define NUMPAD_1 65436
-# define NUMPAD_2 65437
-# define NUMPAD_3 65438
-# define NUMPAD_4 65439
-# define NUMPAD_5 65440
+# define NUMPAD_2 65433
+# define NUMPAD_3 65435
+# define NUMPAD_4 65430
+# define NUMPAD_5 65437
+
+# define ESC	65307
 
 #endif
