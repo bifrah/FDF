cf1529a342e36e8a31d413d22d08f763e7d74cb3
bifrah <benj.ifrah@gmail.com>
2021-11-18 07:11:50 +0100

    suppression commentaire

--- srcs/bresenham.c
+++ srcs/bresenham.c
@@ -16,32 +16,31 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 			{
 				if (dy > 0)
 				{
-					// vecteur oblique dans le 1er cadran
-					if (dx >= dy) // vecteur diagonal ou oblique proche de l’horizontale, dans le 1er octant
+					if (dx >= dy)
 					{
 						e = dx;
 						dx = e * 2;
-						dy = dy * 2;  // e est positif
-						while (x1 != x1 + 1 && x1 != x2)// déplacements horizontaux
+						dy = dy * 2;
+						while (x1 != x1 + 1 && x1 != x2)
 						{
 							ft_draw(x1, y1);
 							e -= dy;
-							if (e < 0)	// déplacement diagonal
+							if (e < 0)
 							{
 								y1 = y1 + 1;
 								e += dx;
 							}
 						}
 					}
-					else	// vecteur oblique proche de la verticale, dans le 2d octant
+					else
 					{
 						dy = (e = dy) * 2;
-						dx = dx * 2;  // e est positif
-						while (y1 != y1 + 1 && y1 != y2) // déplacements verticaux
+						dx = dx * 2;
+						while (y1 != y1 + 1 && y1 != y2)
 						{
 							ft_draw(x1, y1);
 							e -= dx;
-							if (e < 0)	// déplacement diagonal
+							if (e < 0)
 							{
 								x1 = x1 + 1;
 								e += dy;
@@ -49,17 +48,15 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 						}
 					}
 					if (dy < 0 && dx > 0)
-					// vecteur oblique dans le 4e cadran
 					{
 						if (dx >= -dy)
 						{
-							// vecteur diagonal ou oblique proche de l’horizontale, dans le 8e octant
-							dx = (e = dx) * 2 ; dy = dy * 2 ;  // e est positif
-							while (x1 != x1 + 1 && x1 != x2) // déplacements horizontaux
+							dx = (e = dx) * 2 ; dy = dy * 2;
+							while (x1 != x1 + 1 && x1 != x2)
 							{
 								ft_draw(x1, y1);
 								e += dy;
-								if (e < 0)	// déplacement diagonal
+								if (e < 0)
 								{
 									y1 = y1 - 1;
 									e += dx;
@@ -68,15 +65,15 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 
 						}
 					}
-					else  // vecteur oblique proche de la verticale, dans le 7e octant
+					else
 					{
 						dy = (e = dy) * 2;
-						dx = dx * 2 ;  // e est négatif
-						while (y1 != y1 - 1 && y1 != y2)  // déplacements verticaux
+						dx = dx * 2;
+						while (y1 != y1 - 1 && y1 != y2)
 						{
 							ft_draw(x1, y1);
 							e += dx;
-							if (e > 0)	// déplacement diagonal
+							if (e > 0)
 							{
 								x1 = x1 + 1;
 								e += dy;
@@ -86,7 +83,7 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 				}
 				if (dy == 0 && dx > 0)
 				{
-					while (x1 != x1 + 1 && x1 != x2)	// vecteur horizontal vers la droite
+					while (x1 != x1 + 1 && x1 != x2)
 						ft_draw(x1, y1);
 				}
 				if (dx < 0)
@@ -94,73 +91,72 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 					dy = y2 - y1;
 					if (dy != 0)
 					{
-						if (dy > 0)	// vecteur oblique dans le 2d cadran
+						if (dy > 0)
 						{
-							if (-dx >= dy)// vecteur diagonal ou oblique proche de l’horizontale, dans le 4e octant
+							if (-dx >= dy)
 							{
 								dx = (e = dx) * 2;
-								dy = dy * 2;  // e est négatif
-								while (x1 != x1 - 1 && x1 != x2)  // déplacements horizontaux
+								dy = dy * 2;
+								while (x1 != x1 - 1 && x1 != x2)
 									ft_draw(x1, y1);
 								e += dy;
 								if (e >= 0)
 								{
-									y1 = y1 + 1;  // déplacement diagonal
+									y1 = y1 + 1;
 									e += dx;
 								}
 							}
 						}
 					}
 				}
-				else	// vecteur oblique proche de la verticale, dans le 3e octant
+				else
 				{
 					dy = (e = dy) * 2;
-					dx = dx * 2;  // e est positif
-					while (y1 != y1 + 1 && y1 != y2) // déplacements verticaux
+					dx = dx * 2;
+					while (y1 != y1 + 1 && y1 != y2)
 						ft_draw(x1, y1);
 					e += dx;
-					if (e <= 0)	// déplacement diagonal
+					if (e <= 0)
 					{
 						x1 = x1 - 1;
 						e += dy;
 					}
 				}
-				if (dy < 0 && dx < 0)	// vecteur oblique dans le 3e cadran
+				if (dy < 0 && dx < 0)
 				{
-					if (dx <= dy)	// vecteur diagonal ou oblique proche de l’horizontale, dans le 5e octant
+					if (dx <= dy)
 					{
 						dx = (e = dx) * 2;
-						dy = dy * 2;  // e est négatif
-						while (x1 != x1 - 1 && x1 != x2)  // déplacements horizontaux
+						dy = dy * 2;
+						while (x1 != x1 - 1 && x1 != x2)
 						{
 							ft_draw(x1, y1);
 							e -= dy;
 							if (e >= 0)
 							{
-								y1 = y1 - 1;  // déplacement diagonal
+								y1 = y1 - 1;
 								e += dx;
 							}
 						}
 					}
-					else  // vecteur oblique proche de la verticale, dans le 6e octant
+					else
 					{
 						dy = (e = dy) * 2;
-						dx = dx * 2;  // e est négatif
-						while (y1 != y1 - 1 && y1 != y2)  // déplacements verticaux
-							tracePixel(x1, y1);
+						dx = dx * 2;
+						while (y1 != y1 - 1 && y1 != y2)
+							ft_draw(x1, y1);
 						e -= dx;
 						if (e >= 0)
 						{
-							x1 = x1 - 1;  // déplacement diagonal
+							x1 = x1 - 1;
 							e += dy;
 						}
 					}
 				}
 				if (dy == 0 && dx < 0)
 				{	
-					// vecteur horizontal vers la gauche
 					while (x1 != x1 - 1 && x1 != x2)
-						tracePixel(x1, y1);
+						ft_draw(x1, y1);
 				}
 			}
 			if (dx == 0)
@@ -170,18 +166,16 @@ void	tracer_segment(int x1, int y1, int x2, int y2)
 				{
 					if (dy > 0)
 					{
-						while (y1 != y1 + 1 && y1 != y2)// vecteur vertical croissant
-							tracePixel(x1, y1);
+						while (y1 != y1 + 1 && y1 != y2)
+							ft_draw(x1, y1);
 					}
 					if (dy < 0 && dx == 0)
 					{
-						while (y1 != y1 - 1 && y1 != y2)// vecteur vertical décroissant
-							tracePixel(x1, y1);
+						while (y1 != y1 - 1 && y1 != y2)
+							ft_draw(x1, y1);
 					}
 				}
 			}
 		}
 	}
 }
-
-// le pixel final (x2, y2) n’est pas tracé.
